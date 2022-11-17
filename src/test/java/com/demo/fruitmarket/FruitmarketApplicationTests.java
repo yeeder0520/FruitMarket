499affa9de43cbfd7794c7a94b0e066ecdd6c8a4
yeeder0520 <59109960+yeeder0520@users.noreply.github.com>
2022-11-17 23:47:12 +0800

    init project

---
+++ src/test/java/com/demo/fruitmarket/FruitmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.fruitmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
