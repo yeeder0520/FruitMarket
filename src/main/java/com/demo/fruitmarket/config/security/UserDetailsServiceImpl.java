e2e595c778003bb90c48da761a5c95ace42819f6
yeeder0520 <59109960+yeeder0520@users.noreply.github.com>
2023-04-05 22:37:05 +0800

    add spring security

--- src/main/java/com/demo/fruitmarket/config/security/UserDetailsServiceImpl.java
+++ src/main/java/com/demo/fruitmarket/config/security/UserDetailsServiceImpl.java
@@ -23,16 +23,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
-        System.out.println("來惹");
-
-        Optional<UsersPO> byUsername = usersRepo.findByUsername(userId);
-        UsersPO usersPO1 = byUsername.get();
-        System.out.println("usersPO1 = " + usersPO1);
-
         UsersPO usersPO = usersRepo.findByUsername(userId)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with userId: " + userId));
-
-        System.out.println("結束惹");
 
         return User.builder()
                 .username(usersPO.getUsername())
