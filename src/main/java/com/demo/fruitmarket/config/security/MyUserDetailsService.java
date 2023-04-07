65636e15673a5071200c7c3d7661a34cce1a7545
yeeder0520 <59109960+yeeder0520@users.noreply.github.com>
2023-04-07 17:13:23 +0800

    玩玩

---
+++ src/main/java/com/demo/fruitmarket/config/security/MyUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.demo.fruitmarket.config.security;
+
+import com.demo.fruitmarket.entity.UsersPO;
+import com.demo.fruitmarket.repository.UsersRepo;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UsersRepo usersRepo;
+
+    public MyUserDetailsService(UsersRepo usersRepo) {
+        this.usersRepo = usersRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        System.out.println("Start loadUserByUsername userId = " + userId);
+
+        UsersPO usersPO = usersRepo.findByUsername(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with userId: " + userId));
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ADMIN"));
+
+        return new MyUser(usersPO.getUsername(), usersPO.getSecret(), authorities);
+    }
+
+
+}
